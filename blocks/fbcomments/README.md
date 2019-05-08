a51ac294f3e7afbf84d42989a433d874da747496
thiagourias <thiagourias_@hotmail.com>
2019-05-08 15:12:24 -0300

    Teste

---
+++ blocks/fbcomments/README.md
@@ -0,0 +1,63 @@
+Fbcomments
+=====================
+
+Facebook like, share and comment block for Moodle
+
+Installation Instruction
+=====================
+
+* Make sure you have all the required versions.
+* Download and unpack the block folder.
+* Place the folder (eg "myblock") in the "blocks" subdirectory.
+* Visit http://yoursite.com/admin to complete the installation
+* Turn editing on in any home or course page or any other page.
+* Add the block to the page
+* Visit the config link in the block for more options.
+* Please note, due to the way Facebook works, the block probabily wont work well on local installs.
+
+Maturity
+====================
+STABLE
+
+Features
+====================
+* Facebook comment box, with custom number of comments.
+* Facebook like box.
+* Display indivisual box for each page or a single one through out the site.
+* Ability to manage all comments posted using a Fb app id.
+* Supports a dark and light colored theme.
+* Supports a recommend button.
+
+Change log
+=====================
+* 2012070801 - First beta release - 1.0
+* 2013041200 - First public release - 1.1
+* 2013041500 - Added course/mod level urls - 1.2
+* 2013041800 - Support for color themes and Moodle 2.3 -1.3
+* 2013071800 - Added appid support, code cleanup - 1.4
+* 2013071800 - code cleanup - 1.5
+* 2013071800 - Fix missing capabilities strings - 1.6
+* 2013071800 - Add support for ordering of comments - 1.7
+* 2014032000 - Add support for Moodle 2.7 and drop support for Moodle 2.2 - 1.8
+* 2014072100 - Minor clean up and drop support Moodle 2.3 - 1.9
+* 2015011200 - Drop 2.4/2.5, support 2.8, new recommend button, behat tests, travis-ci support - 2.0
+* 2017013000 - Updated supported versions, update travis, api fixes and minor refactor - 2.1
+* 2017101800 - Updated supported versions, update travis, api fixes and minor refactor - 2.2
+* 2018061100 - Version upgrade - 2.3
+* 2019031300 - Version upgrade - 2.4
+
+=====================
+Ankit Kumar Agarwal
+
+Moodle HQ developer
+
+https://github.com/ankitagarwal
+
+http://ankitkumaragarwal.com
+
+ankit.agrr@gmail.com
+
+License
+=====================
+
+GPL 3 or later
---
+++ blocks/fbcomments/edit_form.php
@@ -0,0 +1,109 @@
+<?php
+// This file is part of Fbcomments - https://github.com/ankitagarwal/moodle-block_fbcomments
+//
+// Fbcomments is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Fbcomments is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Form for editing fbcomments block instances.
+ *
+ * @package   block_fbcomments
+ * @copyright 2013 Ankit Agarwal <ankit.agrr@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Form for editing fbcomments block instances.
+ *
+ * @copyright 2013 Ankit Agarwal
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class block_fbcomments_edit_form extends block_edit_form {
+
+    /**
+     * Definition of custom form.
+     *
+     * @param object $mform
+     */
+    protected function specific_definition($mform) {
+
+        // Fields for editing HTML block title and contents.
+        $mform->addElement('header', 'configheader', get_string('blocksettings', 'block'));
+
+        $mform->addElement('text', 'config_title', get_string('configtitle', 'block_fbcomments'));
+        $mform->setType('config_title', PARAM_TEXT);
+        $mform->setDefault('config_title', get_string('newfbblock', 'block_fbcomments'));
+
+        $options = $this->get_page_options();
+        $mform->addElement('select', 'config_urltype', get_string('urltype', 'block_fbcomments'), $options);
+        $options = array('light' => get_string('lightcolor', 'block_fbcomments'), 'dark' => get_string('darkcolor', 'block_fbcomments'));
+        $mform->addElement('select', 'config_colorscheme', get_string('colorscheme', 'block_fbcomments'), $options);
+        $mform->setDefault('config_colorscheme', 'light');
+
+        $options = block_fbcomments::get_button_options();
+        $mform->addElement('select', 'config_enablelike', get_string('enablelike', 'block_fbcomments'), $options);
+        $mform->addElement('advcheckbox', 'config_enableshare', get_string('enableshare', 'block_fbcomments'));
+        $mform->addElement('advcheckbox', 'config_enablecomment', get_string('enablecomment', 'block_fbcomments'));
+        $mform->setType('config_enablelike', PARAM_INT);
+        $mform->setType('config_enablecomment', PARAM_BOOL);
+        $mform->setType('config_enableshare', PARAM_BOOL);
+        $mform->setDefault('config_enablelike', 1);
+        $mform->setDefault('config_enablecomment', 0);
+        $mform->setDefault('config_enableshare', 0);
+        $mform->disabledIf('config_enableshare', 'config_enablelike', 'eq', 0);
+
+        $mform->addElement('text', 'config_numposts', get_string('numposts', 'block_fbcomments'));
+        $mform->setType('config_numposts', PARAM_INT);
+        $mform->addRule('config_numposts', get_string("notnumeric", "block_fbcomments"), "numeric", null, "client");
+        $mform->setDefault('config_numposts', 10);
+        $mform->disabledIf('config_numposts', 'config_enablecomment', 'notchecked');
+
+        $options = block_fbcomments::get_order_by_options();
+        $mform->addElement('select', 'config_commentorder', get_string('commentorder', 'block_fbcomments'), $options);
+        $mform->setType('config_commentorder', PARAM_ALPHA);
+        $mform->addHelpButton('config_commentorder', 'commentorder', 'block_fbcomments');
+        $mform->disabledIf('config_commentorder', 'config_enablecomment', 'notchecked');
+
+        $mform->addElement('text', 'config_appid', get_string('appid', 'block_fbcomments'));
+        $mform->addHelpButton('config_appid', 'appid', 'block_fbcomments');
+        $mform->addRule('config_appid', get_string("notnumeric", "block_fbcomments"), "numeric", null, "client");
+        $mform->setType('config_appid', PARAM_INT);
+        $mform->disabledIf('config_appid', 'config_enablecomment', 'notchecked');
+
+    }
+    // No security checks needed as moodle automatically checks if submitted value for a select is in $options.
+
+    /**
+     * Get page options.
+     *
+     * @return array
+     */
+    private function get_page_options() {
+        $options = array(1 => get_string('thispage', 'block_fbcomments'));
+        $pagecontext = $this->page->context;
+        $coursecontext = $pagecontext->get_course_context(IGNORE_MISSING);
+        if (has_capability('block/fbcomments:manageurl', context_system::instance())) {
+            // Most probably an admin.
+            $options[2] = get_string('siteroot', 'block_fbcomments');
+        }
+        if ($coursecontext && has_capability('block/fbcomments:manageurl', $coursecontext)) {
+            $options[3] = get_string('coursepage', 'block_fbcomments');
+        }
+        if ($pagecontext->contextlevel == CONTEXT_MODULE && has_capability('block/fbcomments:manageurl', $pagecontext)) {
+            $options[4] = get_string('modpage', 'block_fbcomments', $pagecontext->get_context_name(false));
+        }
+        return $options;
+    }
+}
